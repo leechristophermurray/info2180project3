3dfbd61767cd8f52cc80b49f2160d946f65332c4
Alexander Nicholson <alexj.nich@hotmail.com>
2014-12-11 11:13:54 -0500

    Rebased on new design

--- adduser.php
+++ adduser.php
@@ -21,11 +21,10 @@ if ($validform){
 			$db = new PDO("mysql:dbname=test;", "root", "");
 			$statement = $db->prepare("INSERT INTO users (first_name, last_name, password, username) values (:first_name, :last_name, :password, :username)");
 			$statement->execute($form); //Execute prepared query with data
-			header('Location: /homescreen.php')
+			header('Location: /mail.html');
 		} catch (PDOException $e){
 			echo "Connection failed: " . $e->getMessage();
 		}
 
 } else {
-	header('Location: /adduser.html')
 }
